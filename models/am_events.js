ea920b0ba30732f35fd0c87462689c63e2c731c6
Steve Black <SteveBlackecmpe@gmail.com>
2021-03-17 22:10:41 -0400

    updated eslintrc removed camel case error, updated formating to pretty

--- models/am_events.js
+++ models/am_events.js
@@ -1,51 +1,51 @@
 module.exports = (sequelize, DataTypes) => {
-    const Events = sequelize.define('Events', {
-        title: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1],
-            },
-        },
-        assignee: {
-            type: DataTypes.STRING,
-            allowNull: true,
-            validate: {
-                len: [1],
-            },
-        },
-        start_date: {
-            type: DataTypes.DATEONLY,
-            allowNull: false,
-            validate: {
-                len: [1],
-            },
-        },
-        start_time: {
-            type: DataTypes.STRING,
-            allowNull: true,
-        },
-        duration: {
-            type: DataTypes.INTEGER,
-            allowNull: true,
-        },
-        end_date: {
-            type: DataTypes.DATEONLY,
-            allowNull: true,
-        },
-        repeat_cycle: {
-            type: DataTypes.STRING,
-            allowNull: true,
-        },
-        reminder: {
-            type: DataTypes.STRING,
-            allowNull: true,
-        },
-        description: {
-            type: DataTypes.TEXT,
-            allowNull: true,
-        },
-    });
+  const Events = sequelize.define("Events", {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    assignee: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+        len: [1]
+      }
+    },
+    start_date: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    start_time: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    duration: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    end_date: {
+      type: DataTypes.DATEONLY,
+      allowNull: true
+    },
+    repeat_cycle: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    reminder: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: true
+    }
+  });
 
-    return Events;
+  return Events;
 };
