1c2981e19b7edaad11aba115cdeefd90e906f70d
Steve Black <SteveBlackecmpe@gmail.com>
2021-03-21 19:24:26 -0400

    Got save button working, add event data into object.

--- routes/events-api.js
+++ routes/events-api.js
@@ -5,9 +5,14 @@ const db = require("../models");
 // =============================================================
 module.exports = (app) => {
   // GET route for getting all of the posts
-  app.get("api/events/", (req, res) => {
-    db.Events.findAll({}).then((dbEvents) => res.json(dbEvents));
-  });
+  
+  // commented out by steve -----------------3-21-21 6pm START
+  // app.get("api/events/", (req, res) => {
+  //   db.Events.findAll({}).then((dbEvents) => res.json(dbEvents));
+  //   // pretty sure this should be events(table)... not dbevents(database)... 
+  // });
+  // commented out by steve -----------------3-21-21 6pm STOP
+
   //don't think we are doing this but will save here incase
   //   // Get route for returning posts of a specific category
   //   app.get('/api/events/category/:category', (req, res) => {
@@ -31,8 +36,9 @@ module.exports = (app) => {
 
   // POST route for saving a new post
   app.post("/api/events", (req, res) => {
-    console.log("app post route fired");
+    console.log("events-api.js app post route fired");
     console.log(req.body);
+    // steve commented this out until the consolelog shows the object ready to push into data base. 3-21-21-6pm Start
     // db.Events.create({
     //   title: req.body.title,
     //   // assignee: req.body.assignee,
@@ -42,6 +48,7 @@ module.exports = (app) => {
     //   // end_date: req.body.end_date,
     //   repeat_cycle: req.body.repeat_cycle,
     //   description: req.body.description
+    // steve commented this out until the consolelog shows the object ready to push into data base. 3-21-21-6pm Stop
   })
   //.then((dbEvents) => res.json(dbEvents)); // had to comment this out to get it to load.. Not sure what is wrong. 
 
