cc1e2e66e689b3274fb80723ab310b5755cc1e37
Sara Taylor <sara.swifti@gmail.com>
2021-03-23 23:54:44 -0400

    adding in JAWS

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,19 @@
 const mysql = require('mysql');
 let connection;
 
-// if (process.env.JAWSDB_URL) {
-//   connection = mysql.createConnection(process.env.JAWSDB_URL);
-// }
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
 
-// else{
- const connection = mysql.createConnection({
+else{
+  connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
     password: process.env.myPassword,
     database: 'dbevents',
 });
-// }
+}
 
 connection.connect((err) => {
   if (err) {
